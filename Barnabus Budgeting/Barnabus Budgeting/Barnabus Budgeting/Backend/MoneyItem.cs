5ef5cef6a9b5e5fa7a6ae2579cea73a1426bf00e
Conner Weatherston <connerweatherston@hotmail.co.uk>
2020-06-18 00:29:03 +0100

    Added list view as a starting point.

---
+++ Barnabus Budgeting/Barnabus Budgeting/Barnabus Budgeting/Backend/MoneyItem.cs
@@ -0,0 +1,27 @@
+﻿// Represents an object.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barnabus_Budgeting.Backend
+{
+    public class MoneyItem
+    {
+        public MoneyItem()
+        {
+        }
+
+        public MoneyItem(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public DateTime Start { set; get; }
+        public DateTime End { set; get; }
+        public float Amount { set; get; }      
+    }
+}
--- Barnabus Budgeting/Barnabus Budgeting/Barnabus Budgeting/MainPage.xaml.cs
+++ Barnabus Budgeting/Barnabus Budgeting/Barnabus Budgeting/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Barnabus_Budgeting.Backend;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,20 @@ namespace Barnabus_Budgeting
         public MainPage()
         {
             InitializeComponent();
+
+            // Define some data.
+            List<MoneyItem> data = new List<MoneyItem>
+            {
+                new MoneyItem("Monday","A"),
+                new MoneyItem("Tuesday","A"),
+                new MoneyItem("Wednesday","A"),
+                new MoneyItem("Thursday","A"),
+                new MoneyItem("Friday","A"),
+                new MoneyItem("Saturday","A"),
+                new MoneyItem("Sunday","A")
+            };
+
+            weekView.ItemsSource = data;
         }
     }
 }
